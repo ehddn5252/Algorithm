924ac540db29d29ec01985a72f09a38b5bb62310
dongwoo kim <51036842+ehddn5252@users.noreply.github.com>
2022-12-26 15:22:53 +0900

    [Silver II] Title: 촌수계산, Time: 108 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,31 +1,25 @@
-from collections import deque
-
 num = int(input())  # 사람수
 a, b = map(int, input().split(" "))  # 촌수를 계산해야 하는 두 사람 번호
 m = int(input())  # 자식간의 관계 수 m
 
 l = [[] for _ in range(num + 1)]
 for i in range(m):
-    input1, input2 =  map(int, input().split(" "))
+    input1, input2 = map(int, input().split(" "))
     l[input1].append(input2)
     l[input2].append(input1)
 
-q = deque([[a, 0]])
 visit = [False for i in range(num + 1)]
-break_flag = False
 
-while q:
-    popped = q.popleft()
-    node = popped[0]
-    visit[node] = True
-    ret = popped[1]
-    if node == b:
-        print(ret)
-        break_flag = True
-        break
-    for i in l[node]:
+def dfs(v, cnt):
+    cnt += 1
+    visit[v] = True
+
+    for i in l[v]:
+        if i == b:
+            print(cnt - 1)
+            exit()
         if not visit[i]:
-            q.append([i, ret + 1])
+            dfs(i, cnt)
 
-if not break_flag:
-    print(-1)
+dfs(a, 1)
+print(-1)
