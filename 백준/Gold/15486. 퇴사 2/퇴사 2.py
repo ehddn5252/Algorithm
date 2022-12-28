c3906c206755348286b5562549a0102cc25606fd
dongwoo kim <51036842+ehddn5252@users.noreply.github.com>
2022-12-28 19:50:22 +0900

    [Gold V] Title: 퇴사 2, Time: 1088 ms, Memory: 127572 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,16 +1,12 @@
 import sys
 
 n = int(input())
-l = []
 dp = [0 for i in range(n + 1)]
 for i in range(n):
-    l.append(list(map(int, sys.stdin.readline().split(" "))))
-for i in range(n):
-    days = l[i][0]
-    cost = l[i][1]
+    days,cost = map(int, sys.stdin.readline().split(" "))
     for j in range(days):
         if i + j <= n:
             dp[i + j] = max(dp[i], dp[i + j])
         if i + days <= n:
-            dp[i + days] = max(dp[i + days], dp[i + j], dp[i] + cost)
+            dp[i + days] = max(dp[i + days], dp[i] + cost)
 print(max(dp))
