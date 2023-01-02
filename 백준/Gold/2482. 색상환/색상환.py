abaf98ff9e9402971e945f5a207692278c00f764
dongwoo kim <51036842+ehddn5252@users.noreply.github.com>
2023-01-02 21:37:45 +0900

    [Gold IV] Title: 색상환, Time: 132 ms, Memory: 121136 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.py
+++ 백준/Gold/2482. 색상환/색상환.py
@@ -1,14 +1,8 @@
-dp = [[0 for i in range(1001)] for _ in range(1001)]
-
 N = int(input())
 K = int(input())
 
-for i in range(1001):
-    dp[i][0] = 1
-    dp[i][1] = i
-
-for n in range(2, 1001):
+dp = [[1 if j == 0 else i if j == 1 else 0 for j in range(N + 1)] for i in range(N + 1)]
+for n in range(2, N + 1):
     for k in range(2, n):
         dp[n][k] = (dp[n - 2][k - 1] + dp[n - 1][k]) % 1000000003
-
 print((dp[N - 3][K - 1] + dp[N - 1][K]) % 1000000003)
