8a6b5d07c7c641f0c4c36d8d8cd113529122dbe7
dongwoo kim <sunbi5252@naver.com>
2023-01-24 17:39:10 +0900

    [Gold V] Title: 리모컨, Time: 264 ms, Memory: 115268 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,17 +1,15 @@
 target: int = int(input())
 N: int = int(input())
-l=[]
-if N!=0:
+l = []
+if N != 0:
     l = list(map(int, input().split(" ")))
 
-min_num = abs(100-target)
-
-for n in range(1000001):
-    digit_check = str(n)
-
-    for j in range(len(digit_check)):
-        if int(digit_check[j]) in l:
+min_value = abs(100-target)
+for i in range(1000001):
+    digit = str(i)
+    for j in range(len(digit)):
+        if int(digit[j]) in l:
             break
-        elif j==len(digit_check)-1:
-            min_num = min(min_num,abs(n - target)+len(digit_check))
-print(min_num)
+        elif j == len(digit) - 1:
+            min_value = min(min_value, abs(i - target) + len(digit))
+print(min_value)
