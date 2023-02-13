80b9c34e0f281360554ba0c076960e4cfda55be8
dongwoo kim <sunbi5252@naver.com>
2023-02-13 16:58:46 +0900

    [Gold IV] Title: 해킹, Time: 744 ms, Memory: 151576 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.py
+++ 백준/Gold/10282. 해킹/해킹.py
@@ -1,32 +1,24 @@
-"""
-1. 처음 감염된 컴퓨터로 BFS 돌린다.
-
-"""
+testcase = int(input())
+import sys
 import heapq
 
-tc = int(input())
-
-for _ in range(tc):
-    n, d, c = map(int, input().split(" "))  # 컴퓨터 수, 의존성 개수, 해킹 당한 컴퓨터 수
+for _ in range(testcase):
+    n, d, c = map(int, input().split(" "))  # 컴퓨터 개수, 의존성 개수, 해킹 컴퓨터 번호
     l = [[] for _ in range(n + 1)]
+    for i in range(d):
+        a, b, t = map(int, sys.stdin.readline().split())
+        l[b].append((t, a))
     visit = [False for _ in range(n + 1)]
-    for t in range(d):
-        a, b, s = map(int, input().split(" "))  # 컴퓨터 수, 의존성 개수, 해킹 당한 컴퓨터 수
-        l[b].append((s,a))
-
-    q = [(0, c)]
-    heapq.heapify(q)
-    max_times = 0
+    q = [(0, c)]  # (time, computer number)
+    max_times=0
     while q:
-        time,node = heapq.heappop(q)
-        if visit[node]:
+        time, c_n = heapq.heappop(q)
+        if visit[c_n]:
             continue
-        else:
-            visit[node]=True
+        visit[c_n] = True
         if time > max_times:
-            max_times = time
-
-        for i in l[node]:
-            if not visit[i[1]]:
-                heapq.heappush(q, (time + i[0],i[1]))
-    print(str(sum(visit)) + " " + str(max_times))
+            max_times=time
+        for i, v in enumerate(l[c_n]):
+            if not visit[v[1]]:
+                heapq.heappush(q, (time + v[0], v[1]))
+    print(f'{sum(visit)} {max_times}')
