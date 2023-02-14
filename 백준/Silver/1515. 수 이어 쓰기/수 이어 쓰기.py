ce250883304162d16cf5b2c574e1de458f57335c
dongwoo kim <sunbi5252@naver.com>
2023-02-14 18:33:38 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 4140 ms, Memory: 244984 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
@@ -0,0 +1,20 @@
+n = input()
+nums = ""
+check_ans = [0]
+for i in range(1, 100000):
+    nums += str(i)
+    check_ans.append(len(str(i)))
+pointer = 0
+ans=1
+for num, i in enumerate(nums):
+    if pointer == len(n):
+        ans = num
+        break
+    if n[pointer] == i:
+        pointer += 1
+stacked = 0
+for i in range(1, len(check_ans)):
+    stacked += check_ans[i]
+    if ans<=stacked:
+        print(i)
+        break
