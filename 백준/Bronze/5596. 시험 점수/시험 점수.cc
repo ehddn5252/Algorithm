c1b6103f035cd6a77eb23c82d27d1fc3e648c878
dongwoo kim <sunbi5252@naver.com>
2023-04-20 16:04:15 +0900

    [Bronze IV] Title: 시험 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5596. 시험 점수/시험 점수.cc
+++ 백준/Bronze/5596. 시험 점수/시험 점수.cc
@@ -4,20 +4,16 @@ using namespace std;
 
 
 int main() {
-	int a_score = 0, a, b, c, d;
-	int b_score = 0;
-
-	cin >> a >> b >> c >> d;
-	a_score += a;
-	a_score += b;
-	a_score += c;
-	a_score += d;
-	cin >> a >> b >> c >> d;
-
-	b_score += a;
-	b_score += b;
-	b_score += c;
-	b_score += d;
+	int a_score = 0, b_score = 0, num;
+	for (int i = 0; i < 4; ++i) {
+		cin >> num;
+		a_score += num;
+	}
+	
+	for (int i = 0; i < 4; ++i) {
+		cin >> num;
+		b_score += num;
+	}
 
 	if (a_score >= b_score) {
 		cout << a_score;
