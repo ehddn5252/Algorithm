f7a5cc4b68d819ffa01822eac6cb3f88fd1e0220
dongwoo kim <sunbi5252@naver.com>
2023-04-20 16:33:33 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+
+	int a, b,a1,a2,a3,a4,a5;
+	cin >> a >> b;
+
+	a *= b;
+
+	cin >> a1 >> a2 >> a3 >> a4 >> a5;
+	cout << a1 - a << " " << a2 - a << " " << a3 - a << " " << a4 - a << " " << a5 - a;
+	return 0;
+}
