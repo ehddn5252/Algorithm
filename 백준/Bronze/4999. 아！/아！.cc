a9e2f4c86cbb0870a3ee8885d9fcbf17257b0b6e
dongwoo kim <sunbi5252@naver.com>
2023-04-20 16:27:53 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,22 @@
+
+
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+
+	string a, b;
+	cin >> a;
+	cin >> b;
+
+	if (a.size() >= b.size() ){
+		cout<<"go";
+	}
+	else {
+		cout<<"no";
+	}
+	return 0;
+	
+}
