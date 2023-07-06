7ac15665fb0626be1827e788220b1f8cafac0ef5
dongwoo kim <sunbi5252@naver.com>
2023-07-06 13:35:56 +0900

    [Gold IV] Title: 부분합, Time: 32 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -2,39 +2,34 @@
 #include<vector>
 using namespace std;
 
+
+
 /* 아이디어
-* 
+*
 * 1. 왼쪽에서부터 부분합의 길이를 잰다.
 * 2. 부분합의 합을 초과하면 앞에서부터 자른다.
 * 3. 초과하지 않으면 뒤에서부터 더한다.
-*/
 
+*/
 #define ANS_MAX 2147483647
 int main() {
-	int N,S;
+
+	int N, S;
+
 	cin >> N >> S;
 	vector<long long> v(N);
-	for (int i = 0; i < N; ++i) cin >> v[i];
+	for (int i = 0; i < N; ++i) {
+		cin >> v[i];
+	}
 
-	long long start = 0, end = 0,ans = ANS_MAX, nowLength = 0,nowNum=0;
-	while (end <= N && start<=N) {
-		if (nowNum >= S){
+	long long start = 0, end = 0, ans = ANS_MAX, nowLength = 0, nowNum = 0;
+	while (end <= N && start < N) {
+		if (nowNum >= S) {
+			nowNum -= v[start++];
 			if (ans > nowLength) {
 				ans = nowLength;
 			}
-			while (start < end) {
-				nowNum -= v[start];
-				start += 1;
-				nowLength -= 1;
-				if (nowNum < S) {
-					break;
-				}
-				else {
-					if (ans > nowLength) {
-						ans = nowLength;
-					}
-				}
-			}
+			nowLength -= 1;
 		}
 		else {
 			if (end == N) {
@@ -48,5 +43,6 @@ int main() {
 		ans = 0;
 	}
 	cout << ans;
+
 	return 0;
 }
